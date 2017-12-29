8e455d42e9b59de0d38b60f25a6cd27bf1004f90
wangxn <wangxn@feezu.cn>
2017-12-29 18:18:19 +0800

    逻辑回归, 梯度递增优化算法, 第一个简单例子

--- logistic_regression/log_regres.py
+++ logistic_regression/log_regres.py
@@ -8,7 +8,7 @@ def load_data_set():
     fr = open('testSet.txt')
     for line in fr.readlines():
         line_arr = line.strip().split()
-        data_mat.append([1.0, float(line_arr[0])], float(line_arr[1]))
+        data_mat.append([1.0, float(line_arr[0]), float(line_arr[1])])
         label_mat.append(int(line_arr[2]))
 
     return data_mat, label_mat
@@ -58,3 +58,8 @@ def grad_ascent(data_mat_in, class_labels):
         weights = weights + alpha * data_matrix.transpose() * error
 
     return weights
+
+
+data_mat, label_mat = load_data_set()
+print("计算之后,调整了的回归系数")
+print(grad_ascent(data_mat, label_mat))
